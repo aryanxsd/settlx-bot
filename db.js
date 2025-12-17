5cd42a5405fe1a39fbfb943e39d8b04417ebf865
aryanxsd <167317721+aryanxsd@users.noreply.github.com>
2025-12-18 04:44:02 +0530

    Remove DATABASE_URL check from db.js
    Removed error check for DATABASE_URL in db.js.

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
